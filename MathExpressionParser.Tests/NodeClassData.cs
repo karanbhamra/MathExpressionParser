9535cf8665702e3be4414724a86d4a44c43c874a
Karan Bhamra <karan.bhamra@gmail.com>
2020-07-15 17:23:07 -0700

    Moved eval class data file to tests folder

--- MathExpressionParser.Tests/NodeClassData.cs
+++ MathExpressionParser.Tests/NodeClassData.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
-namespace MathExpressionParser
+namespace MathExpressionParser.Tests
 {
     public class NodeClassData : IEnumerable<object[]>
     {
